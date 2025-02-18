1385b48397d5e70187fad0b32be0c4067b0f446b
orafasantos <elrafsantos@gmail.com>
2025-02-18 02:59:54 -0300

    Day 1 finished

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,13 +1,12 @@
 import { ComponentProps, ReactNode } from "react";
 
-interface ButtonProps extends ComponentProps<"button"> { }
+interface ButtonProps extends ComponentProps<"button"> {}
 
 export function Button(props: ButtonProps) {
-    return (
-
-        <button
-            className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer hover:bg-blue transition-colors duration-300 hover:text-gray-900"
-            {...props}
-        />
-    );
+	return (
+		<button
+			className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer hover:bg-blue transition-colors duration-300 hover:text-gray-900"
+			{...props}
+		/>
+	);
 }
--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -5,10 +5,10 @@ interface IconButtonProps extends ComponentProps<"button"> {}
 }
 
 export function IconButton(props: IconButtonProps) {
-  return (
-    <button
-      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer hover:bg-blue transition-colors duration-300 hover:text-gray-900"
-      {...props}
-    />
-  );
+	return (
+		<button
+			className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer hover:bg-blue transition-colors duration-300 hover:text-gray-900"
+			{...props}
+		/>
+	);
 }
