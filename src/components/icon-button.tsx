b3d41eea627bc6818b15e817fa5d9af3640cce64
orafasantos <elrafsantos@gmail.com>
2025-02-17 22:53:42 -0300

    Components button e button icon Set

---
+++ src/components/icon-button.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+interface IconButtonProps {
+  children: ReactNode;
+}
+
+export function IconButton(props: IconButtonProps) {
+  return (
+    <button className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer hover:bg-blue transition-colors duration-300 hover:text-gray-900">
+      {props.children}
+    </button>
+  );
+}
