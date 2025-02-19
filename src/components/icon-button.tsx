23ffdeb8ceeae1778d53b1700bad75f984a65e71
orafasantos <elrafsantos@gmail.com>
2025-02-19 02:20:34 -0300

    Layout second screen ready.

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,13 +1,15 @@
-import { ComponentProps, ReactNode } from "react";
+import { type ComponentProps, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface IconButtonProps extends ComponentProps<"button"> {}
-{
-}
 
-export function IconButton(props: IconButtonProps) {
+export function IconButton({ className, ...props }: IconButtonProps) {
 	return (
 		<button
-			className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer hover:bg-blue transition-colors duration-300 hover:text-gray-900"
+			className={twMerge(
+				"p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer hover:bg-blue transition-colors duration-300 hover:text-gray-900",
+				className,
+			)}
 			{...props}
 		/>
 	);
